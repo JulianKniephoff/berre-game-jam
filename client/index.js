b25c44c41912459f46bde43dd4cf826979b17ef5
Julian Kniephoff <me@juliankniephoff.com>
2021-01-16 14:04:32 +0100

    Move the player

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import World from '../game/world';
 import State from '../game/state';
+import simulate from '../game/simulation';
 
 import render from './renderer';
 
@@ -13,9 +14,41 @@ import DUMMY_WORLD from '../server/worlds/0.json';
 const world = new World(DUMMY_WORLD);
 const state = new State(world);
 
-// TODO Simulation loop/framerate stuff
+// TODO Where to put this?
+window.addEventListener('keydown', e => {
+    switch (e.code) {
+    case "KeyA":
+    case "ArrowLeft":
+        // Handle "turn left"
+        state.moving = -1;
+        break;
+    case "KeyD":
+    case "ArrowRight":
+        // Handle "turn right"
+        state.moving = 1;
+        break;
+    }
+});
+
+window.addEventListener('keyup', e => {
+    switch (e.code) {
+    case "KeyA":
+    case "ArrowLeft":
+        // Handle "turn left"
+        state.moving = 0;
+        break;
+    case "KeyD":
+    case "ArrowRight":
+        // Handle "turn right"
+        state.moving = 0;
+        break;
+    }
+});
+
 // TODO Do we want a service worker?
 const mainLoop = t => {
+    // TODO We probably want to decouple this from rendering ...
+    simulate(t, state);
     render(ctx, t, state);
     requestAnimationFrame(mainLoop);
 };
--- game/state.js
+++ game/state.js
@@ -4,5 +4,6 @@ export default class State {
     constructor(world) {
         this.world = world;
         this.player = new Player({ position: this.world.spawn });
+        this.moving = 0;
     }
 }
