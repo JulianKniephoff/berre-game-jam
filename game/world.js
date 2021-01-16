923933dadf5cf6469cd020bea02510cc6d07443c
Julian Kniephoff <me@juliankniephoff.com>
2021-01-16 13:29:27 +0100

    Refactor `world`

--- game/world.js
+++ game/world.js
@@ -1,23 +1,17 @@
 class World {
-    data = {
-        "platforms": [],
-        "spawn": null,
-        "loaded": false
-    }
+    platforms = [];
+    spawn = null;
+    loaded = false
 
-    fromJson(json) {
-        const out = new World();
-        out.data.platforms = json.platforms;
-        out.data.spawn = json.spawn;
-        out.data.loaded = true;
-        return out;
-    };
+    constructor(json) {
+        Object.assign(this, json);
+    }
 
     toJson() {
         return {
-            "platforms": this.data.platforms,
-            "spawn": this.data.spawn
-        }
+            platforms: this.platforms,
+            spawn: this.spawn
+        };
     }
 
     deepestPlatform() {
