5767ef3ae2a5725b86275750ba8fe2a6921fe68e
Julian Kniephoff <me@juliankniephoff.com>
2021-01-16 12:26:20 +0100

    Fix canvas resolution

--- client/index.js
+++ client/index.js
@@ -1,26 +1,6 @@
 const screen = document.getElementById("screen");
 const ctx = screen.getContext("2d");
 
-const resize = () => {
-    const { width, height } = document.body.getBoundingClientRect();
-    requestAnimationFrame(t => {
-        // TODO How do we want to size this?
-        //   Do we want to keep a specific aspect ratio?
-        //   or a specific size, even?
-        //   How do we want to deal with HiDPI?
-
-        screen.width = Math.floor(width * devicePixelRatio);
-        screen.height = Math.floor(height * devicePixelRatio);
-        screen.style.width = `${screen.width / devicePixelRatio}px`;
-        screen.style.height = `${screen.height / devicePixelRatio}px`;
-
-        render(ctx, t);
-    });
-};
-
-window.addEventListener("resize", resize);
-resize();
-
 const render = (ctx, t) => {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
